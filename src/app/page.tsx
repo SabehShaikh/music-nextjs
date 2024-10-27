f3e0157f46c208ed71b336fc5234c8856cac9e40
SabehShaikh <sabehshaikh201@gmail.com>
2024-10-27 20:23:44 +0500

    add Animated ToolTip and wavy background.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
+import Instructors from "@/components/Instructors";
 import MusicSchoolTestimonial from "@/components/TestimonialCards";
 import HeroSection from "@/components/ui/HeroSection";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
 import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
@@ -10,7 +12,8 @@ export default function Home() {
       <FeaturedCourses />
       <WhyChooseUs />
       <MusicSchoolTestimonial />
+      <UpcomingWebinars />
+      <Instructors />
     </main>
-
   );
 }
